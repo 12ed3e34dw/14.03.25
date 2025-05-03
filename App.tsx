f509474b031666becaf7fc33bc82612da7f7595c
12ed3e34dw <pvlad8056@gmail.com>
2025-05-04 00:22:18 +0300

    init

--- App.tsx
+++ App.tsx
@@ -1,38 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 function App(): React.JSX.Element {
+    const [activeItems, setActiveItems] = useState([false, false, false, false]);
 
-//
-
+    const handlePress = (index: number) => {
+        const updated = [...activeItems];
+        updated[index] = !updated[index];
+        setActiveItems(updated);
+    };
 
     return (
         <View style={styles.container}>
             <View style={styles.item}>
                 <Text style={styles.text}>Футболка</Text>
-                <TouchableOpacity style={styles.button} onPress={() => {}}>
-                    <Text style={styles.buttonText }>Добавить в корзину</Text>
+                <TouchableOpacity
+                    style={activeItems[0] ? styles.buttonActive : styles.button}
+                    onPress={() => handlePress(0)}
+                >
+                    <Text style={styles.buttonText}>
+                        {activeItems[0] ? 'В корзине' : 'Добавить в корзину'}
+                    </Text>
                 </TouchableOpacity>
             </View>
 
             <View style={styles.item}>
                 <Text style={styles.text}>Кофта</Text>
-                <TouchableOpacity style={styles.button} onPress={() => {}}>
-                    <Text style={  styles.buttonText }>Добавить в корзину</Text>
+                <TouchableOpacity style={activeItems[1] ? styles.buttonActive : styles.button} onPress={() => handlePress(1)}>
+                    <Text style={styles.buttonText}>
+                        {activeItems[1] ? 'В корзине' : 'Добавить в корзину'}
+                    </Text>
                 </TouchableOpacity>
             </View>
 
             <View style={styles.item}>
                 <Text style={styles.text}>Штаны</Text>
-                <TouchableOpacity style={styles.button} onPress={() => {}}>
-                    <Text style={ styles.buttonText }>Добавить в корзину</Text>
+                <TouchableOpacity style={activeItems[2] ? styles.buttonActive : styles.button} onPress={() => handlePress(2)}>
+                    <Text style={styles.buttonText}>
+                        {activeItems[2] ? 'В корзине' : 'Добавить в корзину'}
+                    </Text>
                 </TouchableOpacity>
             </View>
 
             <View style={styles.item}>
                 <Text style={styles.text}>Шорты</Text>
-                <TouchableOpacity style={styles.button} onPress={() => {}}>
-                    <Text style={ styles.buttonText}>Добавить в корзину</Text>
+                <TouchableOpacity style={activeItems[3] ? styles.buttonActive : styles.button} onPress={() => handlePress(3)}>
+                    <Text style={styles.buttonText}>
+                        {activeItems[3] ? 'В корзине' : 'Добавить в корзину'}
+                    </Text>
                 </TouchableOpacity>
             </View>
         </View>
@@ -67,17 +82,21 @@ const styles = StyleSheet.create({
         paddingHorizontal: 15,
         borderRadius: 5,
     },
+    buttonActive: {
+        paddingTop: 10,
+        backgroundColor: 'gray',
+        paddingVertical: 10,
+        paddingHorizontal: 15,
+        borderRadius: 5,
+    },
     buttonText: {
         color: '#fff',
         fontWeight: 'bold',
     },
-
     // Style Click button
-buttonText_1: {
+    buttonText_1: {
         backgroundColor: 'blue',
-},
-
-
+    },
 });
 
 export default App;
